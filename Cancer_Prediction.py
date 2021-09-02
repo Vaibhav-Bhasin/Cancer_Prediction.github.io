a448f391af7e0f9bd4458967f59250bc9ecb3f43
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-02 21:57:31 +0530

    Update Cancer_Prediction.py

--- Cancer_Prediction.py
+++ Cancer_Prediction.py
@@ -1,1 +1,10 @@
+#  Project -- AI Cancer Prediction
+# malignant and benign
+#1 Collect data 
 
+from sklearn.datasets import load_breast_cancer
+data = load_breast_cancer()
+#print(data["feature_names"])
+features = data["data"]
+outcomes = data["target"]
+print(features)
